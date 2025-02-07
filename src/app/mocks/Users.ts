2bf2a9ae8fe472d4ad0166d7e3ade40a0e683046
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-02-07 13:14:41 +0100

    Ajout des données mocks

---
+++ src/app/mocks/Users.ts
@@ -0,0 +1,15 @@
+export const users = [
+    {
+      id: "1",
+      name: "Alice Dupont",
+      avatar: "/aliceDupond.svg",
+      status: "online",
+    },
+    {
+      id: "2",
+      name: "Bob Martin",
+      avatar: "/bobMartin.svg",
+      status: "offline",
+    },
+  ];
+  
