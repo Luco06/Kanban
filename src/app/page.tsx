384a6321ff9954765addebe02bcbee1eb4c976d3
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-02-07 13:15:28 +0100

    Factorisation du composant Task

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,14 @@ import { ThemeProvider } from "styled-components";
 import GlobalStyles from "@/styles/GlobalStyles";
 import theme from "@/styles/theme";
 import Task from "./components/Task";
+import { tasks } from "./mocks/Tasks";
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
         <GlobalStyles/>
     < >
-      <Task title={"Hello"} taskResume={" Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, commodi."}/>
+      <Task/>
     </>
   </ThemeProvider>
   )
