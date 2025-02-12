55d613892ce6ee9208b095c094ed6e97749b99ce
Luco06 <luc-olivier.yohan@hotmail.fr>
2025-02-12 18:09:24 +0100

    Création des routes users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,6 +1,28 @@
 import { NextResponse } from "next/server";
-import { users } from "@/app/mocks/Users";
+import { PrismaClient } from "@prisma/client";
 
-export function GET () {
-return NextResponse.json(users)
+const prisma = new PrismaClient();
+
+export async function GET() {
+  try {
+    const users = await prisma.user.findMany();
+    return NextResponse.json(users)
+  }catch (error) {
+    return NextResponse.json({error: "Erreur lo'rs du chargement des utilisateurs"}, {status:500})
+  }
+}
+
+export async function POST(req: Request){
+  const {name, avatar} = await req.json();
+  try {
+    const newUser = await prisma.user.create({
+      data :{
+        name,
+        avatar
+      },
+    });
+    return NextResponse.json(newUser, {status: 201});
+  }catch (error){
+    return NextResponse.json({error: "Erreur l'ors de la création d'un utilisateur"}, {status: 500})
+  }
 }
